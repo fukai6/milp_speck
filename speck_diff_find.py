56104cbbba46b50191ca00075286b5fbc5940e76
Friedrich Wiemer <friedrich.wiemer@rub.de>
2018-03-27 15:24:26 +0200

    replaced precompiled MILPSbox.pyc with the two used python functions

--- speck_diff_find.py
+++ speck_diff_find.py
@@ -1,8 +1,4 @@
-from functools import reduce
-import math
-import random
-from MILPSbox import *
-from random import *
+from MILPSbox import three_way_fork, getVariables_From_Constraints
 
 class speck():
     def __init__(self, blocksize):
@@ -12,36 +8,36 @@ class speck():
         assert r >= 1
         if r == 1:
             return ['p'+str(j) for j in range(self.BlockSize)]
-        if r > 1 :      
+        if r > 1:
             return ['p'+str(j)+'Rd'+str(r-1) for j in range(self.BlockSize)]
-        
+
     def rotl(self, X, n, r):
         assert r >= 1
         temp = [None]*n
-        for i in range(n-r) :
+        for i in range(n-r):
             temp[i] = X[i+r]
-        for i in range(n-r,n) :
+        for i in range(n-r,n):
             temp[i] = X[i-n+r]
         return temp
-    
+
     def rotr(self, X, n, r):
         assert r >= 1
         temp = [None]*n
-        for i in range(r) :
+        for i in range(r):
             temp[i] = X[n-r+i]
-        for i in range(r,n) :
+        for i in range(r,n):
             temp[i] = X[i-r]
         return temp
-    
-    def genVars_Objective(self, r) :
+
+    def genVars_Objective(self, r):
+        assert r >= 1
         h = self.BlockSize
         n = int(h/2)
-        assert r >= 1
         return ['pro'+str(i)+'Rd'+str(r-1) for i in range(n-1)]
-    
+
     def genConstraints_of_Round(self, r):
-        assert r>=1
-        constraints = list()
+        assert r >= 1
+        constraints = []
         X = self.genVars_InVars_at_Round(r)
         Y = self.genVars_InVars_at_Round(r+1)
         h = self.BlockSize
@@ -50,40 +46,40 @@ class speck():
         x1 = X[n : 2*n]
         y0 = Y[0 :   n]
         y1 = Y[n : 2*n]
-        if n == 16 :
+        if n == 16:
             x2 = self.rotr(x0, 16, 7)
             x3 = self.rotl(x1, 16, 2)
-        else :
+        else:
             x2 = self.rotr(x0, n, 8)
             x3 = self.rotl(x1, n, 3)
-            
-        constraints = constraints + ConstraintGenerator.xorConstraints(x3, y1, y0)
+
+        constraints += three_way_fork(x3, y1, y0, r)
 
         d = self.genVars_Objective(r)
 
-        for i in range(n-1) :
-            b = [x2[i],x1[i],y0[i]]
-            a = [x2[i+1],x1[i+1],y0[i+1]]
-            constraints = constraints + [a[1]+' - '+a[2]+' + '+d[i]+' >= 0 ']
-            constraints = constraints + [a[0]+' - '+a[1]+' + '+d[i]+' >= 0 ']
-            constraints = constraints + [a[2]+' - '+a[0]+' + '+d[i]+' >= 0 ']
-            constraints = constraints + [a[0]+' + '+a[1]+' + '+a[2]+' + '+d[i]+' <= 3 ']
-            constraints = constraints + [a[0]+' + '+a[1]+' + '+a[2]+' - '+d[i]+' >= 0 ']
-            constraints = constraints + [b[0]+' + '+b[1]+' + '+b[2]+' + '+d[i]+' - '+a[1]+' >= 0 ']
-            constraints = constraints + [a[1]+' + '+b[0]+' - '+b[1]+' + '+b[2]+' + '+d[i]+' >= 0 ']
-            constraints = constraints + [a[1]+' - '+b[0]+' + '+b[1]+' + '+b[2]+' + '+d[i]+' >= 0 ']
-            constraints = constraints + [a[0]+' + '+b[0]+' + '+b[1]+' - '+b[2]+' + '+d[i]+' >= 0 ']
-            constraints = constraints + [a[2]+' - '+b[0]+' - '+b[1]+' - '+b[2]+' + '+d[i]+' >= -2 ']
-            constraints = constraints + [b[0]+' - '+a[1]+' - '+b[1]+' - '+b[2]+' + '+d[i]+' >= -2 ']
-            constraints = constraints + [b[1]+' - '+a[1]+' - '+b[0]+' - '+b[2]+' + '+d[i]+' >= -2 ']
-            constraints = constraints + [b[2]+' - '+a[1]+' - '+b[0]+' - '+b[1]+' + '+d[i]+' >= -2 ']
+        for i in range(n-1):
+            b = [x2[i], x1[i], y0[i]]
+            a = [x2[i+1], x1[i+1], y0[i+1]]
+            constraints += [a[1]+' - '+a[2]+' + '+d[i]+' >= 0 ']
+            constraints += [a[0]+' - '+a[1]+' + '+d[i]+' >= 0 ']
+            constraints += [a[2]+' - '+a[0]+' + '+d[i]+' >= 0 ']
+            constraints += [a[0]+' + '+a[1]+' + '+a[2]+' + '+d[i]+' <= 3 ']
+            constraints += [a[0]+' + '+a[1]+' + '+a[2]+' - '+d[i]+' >= 0 ']
+            constraints += [b[0]+' + '+b[1]+' + '+b[2]+' + '+d[i]+' - '+a[1]+' >= 0 ']
+            constraints += [a[1]+' + '+b[0]+' - '+b[1]+' + '+b[2]+' + '+d[i]+' >= 0 ']
+            constraints += [a[1]+' - '+b[0]+' + '+b[1]+' + '+b[2]+' + '+d[i]+' >= 0 ']
+            constraints += [a[0]+' + '+b[0]+' + '+b[1]+' - '+b[2]+' + '+d[i]+' >= 0 ']
+            constraints += [a[2]+' - '+b[0]+' - '+b[1]+' - '+b[2]+' + '+d[i]+' >= -2 ']
+            constraints += [b[0]+' - '+a[1]+' - '+b[1]+' - '+b[2]+' + '+d[i]+' >= -2 ']
+            constraints += [b[1]+' - '+a[1]+' - '+b[0]+' - '+b[2]+' + '+d[i]+' >= -2 ']
+            constraints += [b[2]+' - '+a[1]+' - '+b[0]+' - '+b[1]+' + '+d[i]+' >= -2 ']
 
-        constraints = constraints + [x2[n-1]+' + '+x1[n-1]+' + '+y0[n-1]+' <= 2 ']
-        constraints = constraints + [x2[n-1]+' + '+x1[n-1]+' + '+y0[n-1]+' - 2 temp'+str(r-1)+' >= 0 ']
-        constraints = constraints + ['temp'+str(r-1)+' - '+x2[n-1]+' >= 0 ']
-        constraints = constraints + ['temp'+str(r-1)+' - '+x1[n-1]+' >= 0 ']
-        constraints = constraints + ['temp'+str(r-1)+' - '+y0[n-1]+' >= 0 ']
-        
+        constraints += [x2[n-1]+' + '+x1[n-1]+' + '+y0[n-1]+' <= 2 ']
+        constraints += [x2[n-1]+' + '+x1[n-1]+' + '+y0[n-1]+' - 2 temp'+str(r-1)+' >= 0 ']
+        constraints += ['temp'+str(r-1)+' - '+x2[n-1]+' >= 0 ']
+        constraints += ['temp'+str(r-1)+' - '+x1[n-1]+' >= 0 ']
+        constraints += ['temp'+str(r-1)+' - '+y0[n-1]+' >= 0 ']
+
         return constraints
 
     def genObjectiveFun_to_Round(self, r):
@@ -97,41 +93,33 @@ class speck():
 
         f = ' + '.join(f)
         return f
-    
+
     def genModel(self, r):
-        V = set([])
         C = list([])
         for i in range(1, r+1):
             C = C + self.genConstraints_of_Round(i)
-        V = BasicTools.getVariables_From_Constraints(C)
         add_constraint_1 = ' + '.join(['p'+str(i) for i in range(self.BlockSize)]) + ' >= 1'
-        V = V.union(BasicTools.getVariables_From_Constraints([add_constraint_1]))
-        filename='diff-round-'+str(r)+'.lp'
-        o=open(filename,'w')
-        o.write('Minimize')
-        o.write('\n')
-        o.write(self.genObjectiveFun_to_Round(r))
-        o.write('\n')
-        o.write('\n')
-        o.write('Subject To')
-        o.write('\n')
-        o.write(add_constraint_1)
-        o.write('\n')
-        for c in C:
-            o.write(c)
+
+        V = getVariables_From_Constraints(C)
+        V = V.union(getVariables_From_Constraints([add_constraint_1]))
+
+        with open('diff-round-'+str(r)+'.lp', 'w') as o:
+            o.write('Minimize\n')
+            o.write(self.genObjectiveFun_to_Round(r))
+            o.write('\n\nSubject To\n')
+            o.write(add_constraint_1)
             o.write('\n')
-        o.write('\n')
-        o.write('\n')
-        o.write('Binary')
-        o.write('\n')
-        for v in V:
-            o.write(v)
-            o.write('\n')
-        o.close()     
+            for c in C:
+                o.write(c + '\n')
+            o.write('\n\nBinary\n')
+            for v in V:
+                o.write(v + '\n')
 
 
 def main():
     print('Initialized...')
+    m = speck(32)
+    m.genModel(4)
 
 if __name__ == '__main__':
     main()
--- speck_line_find.py
+++ speck_line_find.py
@@ -1,8 +1,4 @@
-from functools import reduce
-import math
-import random
-from MILPSbox import *
-from random import *
+from MILPSbox import three_way_fork, getVariables_From_Constraints
 
 class speck():
     def __init__(self, blocksize):
@@ -12,29 +8,29 @@ class speck():
         assert r >= 1
         if r == 1:
             return ['p'+str(j) for j in range(self.BlockSize)]
-        if r > 1 :      
+        if r > 1:
             return ['p'+str(j)+'Rd'+str(r-1) for j in range(self.BlockSize)]
 
     def rotl(self, X, n, r):
         assert r >= 1
         temp = [None]*n
-        for i in range(n-r) :
+        for i in range(n-r):
             temp[i] = X[i+r]
-        for i in range(n-r,n) :
+        for i in range(n-r,n):
             temp[i] = X[i-n+r]
         return temp
 
     def rotr(self, X, n, r):
         assert r >= 1
         temp = [None]*n
-        for i in range(r) :
+        for i in range(r):
             temp[i] = X[n-r+i]
-        for i in range(r,n) :
+        for i in range(r,n):
             temp[i] = X[i-r]
         return temp
 
-    
-    def genVars_Objective(self, r) :
+
+    def genVars_Objective(self, r):
         h = int(self.BlockSize + 1)
         assert r >= 1
         return ['pro'+str(i)+'Rd'+str(r-1) for i in range(h)]
@@ -49,39 +45,39 @@ class speck():
 
         x0 = X[0 : n]
         x1 = X[n: 2*n]
-        
+
         y0 = Y[0 : n]
         y1 = Y[n: 2*n]
-        
-        
-        if n == 16 :
-            x2 = self.yxh(x0, n, 7)
-        else :
-            x2 = self.yxh(x0, n, 8)
+
+
+        if n == 16:
+            x2 = self.rotr(x0, n, 7)
+        else:
+            x2 = self.rotr(x0, n, 8)
 
 
         x4 = ['x4'+'Rd'+str(r-1)+'L'+str(i) for i in range(n)]
         x3 = ['x3'+'Rd'+str(r-1)+'L'+str(i) for i in range(n)]
-        
-        if n == 16 :
+
+        if n == 16:
             y2 = self.rotr(y1, n, 2)
-        else :
+        else:
             y2 = self.rotr(y1, n, 3)
-            
-        constraints = constraints + ConstraintGenerator.xorConstraints(x1, y2, x4)
-        constraints = constraints + ConstraintGenerator.xorConstraints(x3, y1, y0)   
+
+        constraints += three_way_fork(x1, y2, x4, r)
+        constraints += three_way_fork(x3, y1, y0, r)
         d = self.genVars_Objective(r)
-        for i in range(n) :
+        for i in range(n):
             a = [x3[i],x2[i],x4[i]]
-            constraints = constraints + [d[i]+' - '+a[0]+' - '+a[1]+' + '+a[2]+' + '+d[i+1]+' >= 0']
-            constraints = constraints + [d[i]+' + '+a[0]+' + '+a[1]+' - '+a[2]+' - '+d[i+1]+' >= 0']
-            constraints = constraints + [d[i]+' + '+a[0]+' - '+a[1]+' - '+a[2]+' + '+d[i+1]+' >= 0']
-            constraints = constraints + [d[i]+' - '+a[0]+' + '+a[1]+' - '+a[2]+' + '+d[i+1]+' >= 0']
-            constraints = constraints + [d[i]+' + '+a[0]+' - '+a[1]+' + '+a[2]+' - '+d[i+1]+' >= 0']
-            constraints = constraints + [d[i]+' - '+a[0]+' + '+a[1]+' + '+a[2]+' - '+d[i+1]+' >= 0']
-            constraints = constraints + [a[0]+' - '+d[i]+' + '+a[1]+' + '+a[2]+' + '+d[i+1]+' >= 0']
-            constraints = constraints + [d[i]+' + '+a[0]+' + '+a[1]+' + '+a[2]+' + '+d[i+1]+' <= 4']
-            
+            constraints += [d[i]+' - '+a[0]+' - '+a[1]+' + '+a[2]+' + '+d[i+1]+' >= 0']
+            constraints += [d[i]+' + '+a[0]+' + '+a[1]+' - '+a[2]+' - '+d[i+1]+' >= 0']
+            constraints += [d[i]+' + '+a[0]+' - '+a[1]+' - '+a[2]+' + '+d[i+1]+' >= 0']
+            constraints += [d[i]+' - '+a[0]+' + '+a[1]+' - '+a[2]+' + '+d[i+1]+' >= 0']
+            constraints += [d[i]+' + '+a[0]+' - '+a[1]+' + '+a[2]+' - '+d[i+1]+' >= 0']
+            constraints += [d[i]+' - '+a[0]+' + '+a[1]+' + '+a[2]+' - '+d[i+1]+' >= 0']
+            constraints += [a[0]+' - '+d[i]+' + '+a[1]+' + '+a[2]+' + '+d[i+1]+' >= 0']
+            constraints += [d[i]+' + '+a[0]+' + '+a[1]+' + '+a[2]+' + '+d[i+1]+' <= 4']
+
         return constraints
 
     def genObjectiveFun_to_Round(self, r):
@@ -93,44 +89,36 @@ class speck():
                 f.append(self.genVars_Objective(i)[j])
         f = ' + '.join(f)
         return f
-    
+
 
     def genModel(self, r):
         V = set([])
         C = list([])
         for i in range(1, r+1):
             C = C + self.genConstraints_of_Round(i)
-        V = BasicTools.getVariables_From_Constraints(C)
         add_constraint_1 = ' + '.join(['p'+str(i) for i in range(self.BlockSize)]) + ' >= 1'
-        V = V.union(BasicTools.getVariables_From_Constraints([add_constraint_1]))
-        filename='line-'+str(r)+'.lp'
-        o=open(filename,'w')
-        o.write('Minimize')
-        o.write('\n')
-        o.write(self.genObjectiveFun_to_Round(r))
-        o.write('\n')
-        o.write('\n')
-        o.write('Subject To')
-        o.write('\n')
-        o.write(add_constraint_1)
-        o.write('\n')
-        for i in range(r) :
-            o.write('pro0Rd'+str(i)+' = 0')
-            o.write('\n')          
-        for c in C:
-            o.write(c)
+
+        V = getVariables_From_Constraints(C)
+        V = V.union(getVariables_From_Constraints([add_constraint_1]))
+
+        with open('line-round-'+str(r)+'.lp','w') as o:
+            o.write('Minimize\n')
+            o.write(self.genObjectiveFun_to_Round(r))
+            o.write('\n\nSubject To\n')
+            o.write(add_constraint_1)
             o.write('\n')
-        o.write('\n')
-        o.write('\n')
-        o.write('Binary')
-        o.write('\n')
-        for v in V:
-            o.write(v)
-            o.write('\n')
-        o.close()     
-        
+            for i in range(r):
+                o.write('pro0Rd'+str(i)+' = 0\n')
+            for c in C:
+                o.write(c + '\n')
+            o.write('\n\nBinary\n')
+            for v in V:
+                o.write(v + '\n')
+
 def main():
     print('Initialized...')
+    m = speck(32)
+    m.genModel(4)
 
 if __name__ == '__main__':
     main()
